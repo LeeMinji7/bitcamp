4838bab7c67819a4c84280040bdbaa0cae7adff3
bitcamp <bitcamp@DESKTOP-2IKIV7Q>
2021-05-04 17:59:49 +0900

    day04

---
+++ bitcamp-java/src/day04/Book.java
@@ -0,0 +1,27 @@
+package day04;
+
+public class Book {
+	
+	String title;
+	String author;
+	
+	public Book(String t) { // 생성자
+		title = t;
+		author = "작자미상";
+	}
+	
+	public Book(String t, String a) { // 생성자
+		title = t;
+		author = a;
+	}
+
+	public static void main(String[] args) {
+
+		Book littlePrice = new Book("어린왕자", "생텍쥐페리");
+		Book loveStory = new Book("춘향전");
+		
+		System.out.println(littlePrice.title + " " + littlePrice.author);
+		System.out.println(loveStory.title + " " + loveStory.author);
+	}
+
+}
---
+++ bitcamp-java/src/day04/Book2.java
@@ -0,0 +1,36 @@
+package day04;
+
+public class Book2 {
+	
+	String title;
+	String author;
+	
+	public Book2() { // 생성자
+		this("", "");
+	}
+	
+	public Book2(String title) { // 생성자
+		this(title, "작자미상");
+	}
+	
+	public Book2(String title, String author) { // 생성자
+		this.title = title;
+		this.author = author;
+	}
+	
+	void show() {
+		System.out.println(title + " " + author);
+	}
+
+	public static void main(String[] args) {
+
+		Book2 littlePrice = new Book2("어린왕자", "생텍쥐페리");
+		Book2 loveStory = new Book2("춘향전");
+		Book2 emptyBook = new Book2();
+		
+		littlePrice.show();
+		loveStory.show();
+		emptyBook.show();
+	}
+
+}
